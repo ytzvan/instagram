ce3380389923e83ff662cb2b1c9ee74b1bea4dd6
Ytzvan Mastino <mastino14@gmail.com>
2016-02-27 18:57:51 -0500

    added gallery options

--- api/controllers/ItemController.js
+++ api/controllers/ItemController.js
@@ -8,10 +8,11 @@ var http = require('http');
 var ig = require('instagram-node').instagram();
 
 ig.use({ client_id: 'b80cccd26d214e76a0f0ac80ea2abd03',
-         client_secret: '9d0b2dafc8054a34aec67011391033ad' });
-
+		         client_secret: '9d0b2dafc8054a34aec67011391033ad' });
 var redirect_uri = 'http://localhost:1337/item/handleauth';
 var access_token;
+var user;
+var userId;
 
 module.exports = {
 
@@ -23,6 +24,8 @@ module.exports = {
 	},
 
    	authorize : function(req, res) {
+   		ig.use({ client_id: 'b80cccd26d214e76a0f0ac80ea2abd03',
+		         client_secret: '9d0b2dafc8054a34aec67011391033ad' });
   		res.redirect(ig.get_authorization_url(redirect_uri, { scope: ['likes'], state: 'a state' }));
 	},
 
@@ -34,12 +37,36 @@ module.exports = {
 	      console.log(err.body);
 	      res.send("Didn't work");
 	    } else {
-	      access_token = result.access_token;
+	      req.session.access_token = result.access_token;
+	      user = result.username;
+	      userId = result.id;
 	      console.log('Yay! Access token is ', result);
 	      
 	      res.send('You made it!!');
 	    }
 	  });
+
+	},
+
+	getMedia : function (req, res) {
+		
+
+		ig.use({ access_token: req.session.access_token });
+		ig.user_self_media_recent(function(err, medias, pagination, remaining, limit) {
+			var data = {};
+			if (!medias) {
+				return console.log("err", err);
+			} else {
+				for (var i=0; i<medias.length; i++ ){
+					console.log("lenght", medias.length);
+					console.log(medias[i].images.standard_resolution);
+					 data[i] = medias[i].images.low_resolution;
+				}
+				return res.view({fotos: data});
+			}
+		});
+
+
 	}
 
 
